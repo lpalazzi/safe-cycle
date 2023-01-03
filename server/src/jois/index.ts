e30fdacc46c6356173e07814dc39d4ea14388e59
lpalazzi <lpalazzi@outlook.com>
2023-01-03 16:41:51 -0500

    add some geojson schemas to joi

--- server/src/jois/index.ts
+++ server/src/jois/index.ts
@@ -3,9 +3,14 @@ import joi from 'joi';
 declare module 'joi/lib' {
   export interface Root {
     objectId: () => AnySchema;
+    geojson: {
+      position: () => AnySchema;
+      lineString: () => AnySchema;
+    };
   }
 }
 
 export default () => {
   joi.objectId = require('joi-objectid')(joi);
+  joi.geojson = require('./geojson');
 };
