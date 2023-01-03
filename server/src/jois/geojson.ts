ae0fa665c689366b2d307b9e04cec9043659a6d4
lpalazzi <lpalazzi@outlook.com>
2023-01-03 16:41:51 -0500

    add some geojson schemas to joi

---
+++ server/src/jois/geojson.ts
@@ -0,0 +1,8 @@
+import joi from 'joi';
+
+export const position = joi.array().items(joi.number()).min(2).max(3);
+
+export const lineString = joi.object({
+  type: joi.string().equal('LineString').required(),
+  coordinates: joi.array().items(position).required(),
+});
