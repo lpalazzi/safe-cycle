3cd329a6e0ecb7a1e0ad8a9346cb35b62a8d5153
lpalazzi <lpalazzi@outlook.com>
2023-01-06 16:01:42 -0500

    add logo

--- client/src/components/shell/SidebarHeader.tsx
+++ client/src/components/shell/SidebarHeader.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { ActionIcon, Box, Group, Text, Tooltip } from '@mantine/core';
+import { ActionIcon, Box, Group, Tooltip } from '@mantine/core';
 import { IconChevronsLeft } from '@tabler/icons';
 import { useGlobalContext } from 'contexts/globalContext';
-// import { Logo } from 'components/brand/Logo';
+import LogoSvg from 'assets/brand/logo-name.svg';
 
 export const SidebarHeader: React.FC = () => {
   const { toggleNavbar } = useGlobalContext();
@@ -11,13 +11,12 @@ export const SidebarHeader: React.FC = () => {
       sx={(theme) => ({
         paddingLeft: theme.spacing.xs,
         paddingRight: theme.spacing.xs,
-        paddingBottom: theme.spacing.lg,
+        paddingBottom: theme.spacing.md,
         borderBottom: `1px solid ${theme.colors.gray[2]}`,
       })}
     >
       <Group position='apart'>
-        {/* <Logo height='25px' /> */}
-        <Text size={26}>SafeCycle</Text>
+        <img src={LogoSvg} style={{ maxHeight: '50px', maxWidth: '100%' }} />
         <Tooltip label='Collapse sidebar' position='bottom'>
           <ActionIcon onClick={() => toggleNavbar()} size='lg'>
             <IconChevronsLeft color='black' size={26} />
