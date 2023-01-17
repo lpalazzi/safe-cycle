44ed467b6ef9c664f70b23d4c5664e2c454d52ba
lpalazzi <lpalazzi@outlook.com>
2023-01-17 13:28:12 -0500

    fix sidebar fetching on every open

--- client/src/components/shell/Sidebar.tsx
+++ client/src/components/shell/Sidebar.tsx
@@ -8,59 +8,63 @@ import { IconMap2, IconMenu2 } from '@tabler/icons';
 
 export const Sidebar: React.FC = () => {
   const { isMobileSize, isNavbarOpen, toggleNavbar } = useGlobalContext();
-  return isNavbarOpen ? (
-    <Navbar
-      p='md'
-      hiddenBreakpoint='sm'
-      hidden={!isNavbarOpen}
-      width={{ sm: 400 }}
-      height={'100%'}
-    >
-      <Navbar.Section>
-        <SidebarHeader />
-      </Navbar.Section>
-      <Divider my='sm' />
-      <Navbar.Section grow component={ScrollArea}>
-        <SidebarContent />
-      </Navbar.Section>
-      {isMobileSize ? (
-        <Button
-          fullWidth
-          size='lg'
-          leftIcon={<IconMap2 size={18} />}
-          onClick={toggleNavbar}
-          mt='sm'
+  return (
+    <>
+      <Navbar
+        p='md'
+        hiddenBreakpoint='sm'
+        hidden={!isNavbarOpen}
+        width={{ sm: 400 }}
+        height={'100%'}
+        style={isNavbarOpen ? {} : { display: 'none' }}
+      >
+        <Navbar.Section>
+          <SidebarHeader />
+        </Navbar.Section>
+        <Divider my='sm' />
+        <Navbar.Section grow component={ScrollArea}>
+          <SidebarContent />
+        </Navbar.Section>
+        {isMobileSize ? (
+          <Button
+            fullWidth
+            size='lg'
+            leftIcon={<IconMap2 size={18} />}
+            onClick={toggleNavbar}
+            mt='sm'
+          >
+            View map
+          </Button>
+        ) : null}
+        <Divider my='sm' />
+        <Navbar.Section>
+          <SidebarFooter />
+        </Navbar.Section>
+      </Navbar>
+      {isNavbarOpen ? null : (
+        <ActionIcon
+          onClick={() => toggleNavbar()}
+          size='xl'
+          variant='default'
+          style={
+            isMobileSize
+              ? {
+                  position: 'absolute',
+                  top: 10,
+                  right: 10,
+                  zIndex: 10,
+                }
+              : {
+                  position: 'absolute',
+                  top: 20,
+                  left: 20,
+                  zIndex: 10,
+                }
+          }
         >
-          View map
-        </Button>
-      ) : null}
-      <Divider my='sm' />
-      <Navbar.Section>
-        <SidebarFooter />
-      </Navbar.Section>
-    </Navbar>
-  ) : (
-    <ActionIcon
-      onClick={() => toggleNavbar()}
-      size='xl'
-      variant='default'
-      style={
-        isMobileSize
-          ? {
-              position: 'absolute',
-              top: 10,
-              right: 10,
-              zIndex: 10,
-            }
-          : {
-              position: 'absolute',
-              top: 20,
-              left: 20,
-              zIndex: 10,
-            }
-      }
-    >
-      <IconMenu2 color='black' size={32} />
-    </ActionIcon>
+          <IconMenu2 color='black' size={32} />
+        </ActionIcon>
+      )}
+    </>
   );
 };
