6a0ac1cb495b66cd852dc40acfde3f508c4ce4c3
lpalazzi <lpalazzi@outlook.com>
2023-02-13 17:55:14 -0500

    add new route preferences (frontend)

--- client/src/types/Routing.ts
+++ client/src/types/Routing.ts
@@ -1,5 +1,8 @@
 export type RouteOptions = {
   avoidUnsafe?: boolean;
+  stickToCycleRoutes?: boolean;
+  preferPaved?: boolean;
   avoidUnpaved?: boolean;
+  avoidUnpavedB?: boolean;
   alternativeidx?: 0 | 1 | 2 | 3;
 };
