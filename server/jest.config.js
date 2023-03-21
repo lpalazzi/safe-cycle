168a878d177989be4c3bbfdcaa489bc083e9127d
lpalazzi <lpalazzi@outlook.com>
2023-03-21 10:00:18 -0400

    modify jest config

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,8 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFiles: ['dotenv/config'],
-  setupFilesAfterEnv: ['./test/jest.init.ts'],
-  rootDir: 'src',
-  modulePaths: ['<rootDir>'],
+  setupFilesAfterEnv: ['./src/test/jest.init.ts'],
+  modulePaths: ['./src'],
 };
