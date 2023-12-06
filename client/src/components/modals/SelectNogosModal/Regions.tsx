b809c99625c2a271551e1b21d603abc1aced3fe8
lpalazzi <lpalazzi@outlook.com>
2023-12-06 15:18:55 -0500

    render region sort select in portal

--- client/src/components/modals/SelectNogosModal/Regions.tsx
+++ client/src/components/modals/SelectNogosModal/Regions.tsx
@@ -111,6 +111,7 @@ export const Regions: React.FC<{
               if (value && ['alpha', 'location', 'nogoLength'].includes(value))
                 setSortMethod(value as SortMethod);
             }}
+            withinPortal
           />
         </Group>
       </Group>
